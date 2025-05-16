edc2f067b574c483f19d7702687f63416e5d5699
remee-s <rudfla5927@naver.com>
2025-05-16 17:57:15 +0900

    test250516

---
+++ test/js/header_250516.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    
+/*
+    header에 fixed 클래스 주는것...
+    1. 브라우저가 조금이라도 스크롤이 되면 header에 fixed 클래스를 추가
+       -스크롤 값이 0보다 크면(0포함 X 0 초과)
+    2. 다시 브라우저 맨 위로 스크롤이 되면 header에 fixed 클래스를 삭제
+       -스크롤 값이 0이거나 그보다 작은면 
+
+    글래스를 주는 명령
+
+    브라우저가 스크롤 되는 것을 체크할 수 있는 명령을 알아야 함
+
+
+*/
+    //$('header').addClass('fixed')
+    //$('header').removeClass('fixed')
+
+    //html이 로딩된 이후 단 1번만 실행 하는 것
+    let scrolling //let은 변수를 선언한것 
+    scroll_chk() //함수를 호출하는것 (함수 실행)
+
+    function scroll_chk(){ 
+        //함수 선언(scroll_chk라는 이름의 함수가 있어요!!!)
+        scrolling = $(window).scrollTop()
+        console.log(scrolling)
+
+        if(scrolling > 0){ //스크롤 된 값이 0보다 크면 
+            //console.log('0보다 크다?')
+            $('header').addClass('fixed')
+        }else{ // 그외 나머지 전부 (0이거나 0보다 작으면)
+            //console.log('0이다!')
+            $('header').removeClass('fixed')
+        }
+    }  
+
+    $(window).scroll(function(){
+        //스크롤 할때마다 1번씩 실행 (스크롤을 하지 않으면 실행 하지 않음 !!)
+        scroll_chk()//함수를 호출하는것 (함수 실행)
+    })
+
+})//$(document).ready
