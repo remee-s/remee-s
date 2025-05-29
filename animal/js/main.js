4567986cd692c2579a58b1a043887cb69406b74d
remee-s <rudfla5927@naver.com>
2025-05-29 12:58:01 +0900

    25.05.29_12:57

--- animal/js/main.js
+++ animal/js/main.js
@@ -158,7 +158,7 @@ $(document).ready(function(){
 
     /*************************** find 탭 기능 : 끝 ********************************/
 
-    /*************************** 분양 swiper : 끝 ********************************/
+    /*************************** 분양 swiper : 시작 ********************************/
 
     const adpot_swiper = new Swiper('.adopt .swiper', { /* 팝업을 감싼는 요소의 class명 */
 	slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
@@ -176,5 +176,40 @@ $(document).ready(function(){
             prevEl: '.adopt .list_ctrl .btn_prev',
         },
     });
+    /*************************** 분양 swiper : 끝 ********************************/
+    
+    /*************************** 후기 swiper : 시작 ********************************/
+    const review_swiper = new Swiper('.review .swiper', { /* 팝업을 감싼는 요소의 class명 */
+	slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+	spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            768: {    /* 1024px 이상일때 적용 */
+            slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+            spaceBetween: 24, /* 사이의 여백 */
+            },
+            1024: {    /* 1024px 이상일때 적용 */
+                slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24, /* 사이의 여백 */
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        // autoplay: {  /* 팝업 자동 실행 */
+        //     delay: 2500,
+        //     disableOnInteraction: true,
+        //},
+        navigation: {
+            nextEl: '.review .list .btn_next',
+            prevEl: '.review .list .btn_prev',
+        },
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.swiper-pagination', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+        },
+    });
+    // review_swiper.autoplay.stop();  /* 일시정지 기능 */
+    // review_swiper.autoplay.start();  /* 재생 기능 */
+    /*************************** 후기 swiper : 끝 ********************************/
 
 })//$(document).ready
