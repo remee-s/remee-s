a8b22b7342b17a981a0c93aa3d0b29c807c58a8a
remee-s <rudfla5927@naver.com>
2025-06-20 17:21:47 +0900

    .

---
+++ test/js/interactive.25.06.20.js
@@ -0,0 +1,119 @@
+$(document).ready(function(){ //일회용
+    let scrolling
+    let win_h // 브라우저의 높이값
+
+    let slogan = $('.slogan')
+    let slogan_obj =  $('.slogan p span').lenght
+    let slogan_leng = slogan_obj.lenght
+    let slogan_scroll //슬로건 start에서 부터 스크롤 된 값
+    let slogan_top // 슬로건이 위에서 부터 떨어진 거리
+    let slogan_start // 애니메이션 시작점
+    let slogan_end // 애니메이션 종료점
+    let slogan_w // span 태그의 넓이
+
+    let dark = $('.life')
+    let dark_top //위에서 부터 떨어진 거리
+    let dark_start // 애니메이션 시작점
+
+    let photo = $('.banner .photo')
+    let photo_top
+    let photo_start
+    let photo_end
+    let photo_scroll
+    let photo_w
+
+    // 사진싸이즈가 스크롤 하면 커지는 애니메이션
+    function photo_resize(){
+        photo_top = photo.offset().top
+        photo_start = photo_top - (win_h * 0.7) // 밑에서 30%면 0.7
+        photo_end = photo_top + photo.height() - scrolling - (win_h * 0.7)
+        photo_scroll = (scrolling - photo_top) / (photo_end - photo_start) * 100
+        console.log('start', photo_start, 'end', photo_end, 'scroll', photo_scroll)
+    }
+
+
+    scroll_chk() //브라우저가 로딩됐을때 한번 
+    resize_chk()
+    slogan_ani()
+    dark_bg()
+    photo_resize()
+    $(window).scroll(function(){
+        //스크롤 할때 마다 한번씩
+        scroll_chk()
+        slogan_ani()
+        dark_bg()
+        photo_resize()
+    })
+    $(window).resize(function(){
+        //브라우저가 리사이즈 될때마다 한번씩 실행
+        slogan_ani()
+        resize_chk()
+        dark_bg()
+        photo_resize()
+    })
+
+
+
+    $('.slogan p span').eq(1).hide() //eq(1) = 두번째 span 태그
+    
+    function slogan_ani(){
+
+        slogan_top = slogan.offset().top
+        slogan_start = slogan_top - win_h + (win_h * 0.3)
+        //console.log('scroll', scrolling , 'start', slogan_start)
+        //scroll 값 = start 값 ani 가 시작됨
+        slogan_end = slogan_top + slogan.height() - win_h + (win_h * 0.3)
+        //console.log('scroll', scrolling, 'end', slogan_end)
+        //scroll 값 = end 값 ani 가 종료됨
+        slogan_scroll = (scrolling - slogan_start) / (slogan_end - slogan_start) * 100
+        //console.log(slogan_scroll)
+
+        if(slogan_start > scrolling){
+            //console.log('애니메이션 시작 이전')
+            slogan_obj.width(0)
+        }else if(slogan_end > scrolling){
+            //console.log('애니메이션 중~~~')
+            //slogan_w = 100/slogan_leng
+            for(i = 0; i < slogan_leng; i++){
+                slogan_w = (slogan_scroll - (100/slogan_leng)*i) * slogan_leng
+                if(slogan_w > 100){
+                    slogan_w = 100
+                }
+                slogan_obj.eq(i).width(slogan_w + '%')
+            }
+        }else{
+            //console.log('애니메이션 종료!!!!')
+            slogan_obj.width('100%')
+        }
+
+    }//slogan_ani
+
+    // 배경색 바뀌게 하는것 자연스럽게
+    function dark_bg(){
+        dark_top = dark.offset().top
+        dark_start = dark_top - scrolling - (win_h * 0.5)
+        //console.log('scroll', scrolling, 'top', dark_top, 'start', dark_start)
+        if(dark_start < 0){
+            dark.addClass('dark')
+        }else{
+            dark.removeClass('dark')
+        }
+    }
+
+
+    function scroll_chk(){ // 다회용
+        scrolling = $(window).scrollTop()
+        //console.log(' 슬로건 값!! ',scrolling)
+    }
+
+
+    function resize_chk(){
+        win_h = $(window).height()
+        //console.log('브라우저의 높이', win_h)
+    }
+
+
+
+
+
+})//$(document).ready
