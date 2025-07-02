c8f14fceaa36eb7a1ef3f1f6e582fa468f1752f1
remee-s <rudfla5927@naver.com>
2025-07-02 18:01:28 +0900

    25.07.02

--- michelin/js/main.js
+++ michelin/js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-    console.log('연결되는거니????');
+	//header .total_menu .open
+    //header .total_menu .total_menu_wrap .close
+    $('header .total_menu .open').on('click', function(){
+		$('header .total_menu .total_menu_wrap').addClass('active');	
+	});
+    $('header .total_menu .total_menu_wrap .close').on('click', function(){
+		$('header .total_menu .total_menu_wrap').removeClass('active');	
+	});
 })
