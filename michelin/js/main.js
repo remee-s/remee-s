5df2e6ad1c0f670fd28206d6d22e0f3a168853c6
remee-s <rudfla5927@naver.com>
2025-07-04 11:00:15 +0900

    a

--- michelin/js/main.js
+++ michelin/js/main.js
@@ -15,15 +15,15 @@ $(document).ready(function(){
 	breakpoints: {
 			450: {    /* 640px 이상일때 적용 */
 			slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
-			spaceBetween: 20,
+			spaceBetween: 16,
 			},
 			768: {    /* 640px 이상일때 적용 */
 				slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
-				spaceBetween: 20,
+				spaceBetween: 16,
 			},
 			1260: {    /* 640px 이상일때 적용 */
 			slidesPerView: 5,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
-			spaceBetween: 24,
+			spaceBetween: 20,
 			},
 			1500: {    /* 640px 이상일때 적용 */
 			slidesPerView: 6,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
