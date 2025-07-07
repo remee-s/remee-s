8f448680e6a2453fa4878e6ab8399d97e2b0358f
remee-s <rudfla5927@naver.com>
2025-07-07 14:34:10 +0900

    25.07.07.mechelin

--- michelin/js/main.js
+++ michelin/js/main.js
@@ -15,20 +15,16 @@ $(document).ready(function(){
 	});
 
 
-	const magazine_swiper = new Swiper('.magazine .magazine_swiper', { /* 팝업을 감싼는 요소의 class명 */
-		slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+	const magazine_swiper = new Swiper('.magazine .list .swiper', { /* 팝업을 감싼는 요소의 class명 */
+		slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
 		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
 		breakpoints: {
-			463: {    /* 640px 이상일때 적용 */
-			slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
-			spaceBetween: 16,
-			},
 			768: {    /* 640px 이상일때 적용 */
-				slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
 				spaceBetween: 16,
 			},
 			1260: {    /* 640px 이상일때 적용 */
-			slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+			slidesPerView: 5,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
 			spaceBetween: 20,
 			},
 			1500: {    /* 640px 이상일때 적용 */
