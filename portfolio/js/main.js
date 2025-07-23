d59617655d889e9d0dbece5b981e4d67ff92da2a
remee-s <rudfla5927@naver.com>
2025-07-23 15:21:02 +0900

    .

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,15 +1,3 @@
 
 $(document).ready(function(){
-	const myFullpage = new fullpage('#fullpage', {  /* html에서 페이지 전체를 감싸는 요소 */
-		
-		lockAnchors: true,
-		anchors: ['visual', 'infor', 'capter', 'footer'], /* href="#link1" 이렇게 코딩하면 해당 링크명으로 이동 */
-
-		autoScrolling: true, /* 한페이지씩 스크롤 */
-		scrollHorizontally: true,
-
-		verticalCentered: true, /* 컨텐츠 요소 위아래 가운데 */
-		
-		scrollOverflow: false, /* 컨텐츠가 넘쳐도 스크롤 금지 */
-	});
 })//$(document).ready
